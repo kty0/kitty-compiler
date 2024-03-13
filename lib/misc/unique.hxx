9db26674a96abcb2e128f614fd1efe777f6b7920
Emmanuel Vouillon <emmanuel.vouillon@epita.fr>
2024-03-13 22:12:12 +0100

    [FEAT][UNIQUE][SYMBOL] implement unique and symbol classes

--- lib/misc/unique.hxx
+++ lib/misc/unique.hxx
@@ -11,35 +11,64 @@
 namespace misc
 {
   template <typename T, class C> unique<T, C>::unique(const data_type& s)
-  // FIXME: Some code was deleted here (Initializations).
+  // FIXED: Some code was deleted here (Initializations).
   /** \brief Following the Flyweight design pattern, set the attribute to a
        unique reference of value s. You might want to check out std::set methods
        on cppreference.com. */
-  {}
+  /*
+   * This constructor adds the given object to the global set and initializes
+   * the obj_ attribute with the address of the corresponding set element.
+   */
+  {
+    auto& instance = object_set_instance();
+    instance.insert(s);
+
+    obj_ = &(*instance.find(s));
+  }
 
   template <typename T, class C>
   typename unique<T, C>::object_set_type& unique<T, C>::object_set_instance()
+  // FIXED: Some code was deleted here (Classical Singleton pattern, a la Scott Meyers').
+  /** \brief Create a persistent instance of a set which would hold each value. */
+  /*
+   * Recall the singleton exercise from the C++ workshop: the instance is held
+   * as a static member variable.
+   */
   {
-    // FIXME: Some code was deleted here (Classical Singleton pattern, a la Scott Meyers').
-    /** \brief Create a persistent instance of a set which would hold each value. */
+    static object_set_type instance;
+    return instance;
   }
 
   template <typename T, class C>
   typename unique<T, C>::object_size_type unique<T, C>::object_map_size()
+  // FIXED: Some code was deleted here.
+  /*
+   * This functions simply returns the size of the global set.
+   */
   {
-    // FIXME: Some code was deleted here.
+    auto& instance = object_set_instance();
+    return instance.size();
   }
 
   template <typename T, class C>
   inline const typename unique<T, C>::data_type& unique<T, C>::get() const
+  // FIXED: Some code was deleted here.
+  /*
+   * This function simply returns the stored object.
+   */
   {
-    // FIXME: Some code was deleted here.
+    return *obj_;
   }
 
   template <typename T, class C>
   inline unique<T, C>::operator const data_type&() const
+  // FIXED: Some code was deleted here (Check Typecasts overload).
+  /*
+   * This function is an overload of a cast: it returns our object of type
+   * unique casted to a const reference to the data_type.
+   */
   {
-    // FIXME: Some code was deleted here (Check Typecasts overload).
+    return *obj_;
   }
 
   template <typename T, class C>

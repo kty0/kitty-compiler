9452ed26877ac1b892d75e2099d501ecd19e37d6
Dao-phuong-thao cathy  Nguyen <dao-phuong-thaocathy.nguyen@epita.fr>
2024-04-04 20:40:47 +0200

    feat(object): implement the fixmes

--- src/ast/object-visitor.hxx
+++ src/ast/object-visitor.hxx
@@ -27,31 +27,44 @@ namespace ast
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<ClassTy>& e)
   {
-    // IGNORED: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    e.super_get().accept(*this);
+    e.chunks_get().accept(*this);
   }
 
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodChunk>& e)
   {
-    // IGNORED: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    for (const auto dec : e)
+      dec->accept(*this);
   }
 
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodDec>& e)
   {
-    // IGNORED: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    e.formals_get().accept(*this);
+    e.result_get()->accept(*this);
+    e.body_get()->accept(*this);
   }
 
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodCallExp>& e)
   {
-    // IGNORED: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    const exps_type& args = e.args_get();
+    for (auto it = args.begin(); it != args.end(); it++)
+    {
+      (*it)->accept(*this);
+    }
   }
 
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<ObjectExp>& e)
   {
-    // IGNORED: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    e.type_name_get().accept(*this);
   }
 
 } // namespace ast
